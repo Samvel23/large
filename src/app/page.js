dfd0f524a1dd33b0a972ef3e97cd610b35507e5d
Samvel23 <samvelavdalyan2@gmail.com>
2025-10-25 11:29:22 +0400

    removed expo modal

--- src/app/page.js
+++ src/app/page.js
@@ -7,54 +7,11 @@ import { NewsPage } from "./components/news";
 import { AboutPage } from "./components/about-us";
 
 export default function Home() {
-  const [isModalActive, setIsModalActive] = useState(false);
-
-  useEffect(() => {
-    // Check if the modal was shown during this browser tab session
-    const hasSeenModal = sessionStorage.getItem("hasSeenExpoModal");
-
-    if (!hasSeenModal) {
-      setIsModalActive(true);
-      sessionStorage.setItem("hasSeenExpoModal", "true");
-    }
-  }, []);
-
-  const closeModal = () => {
-    setIsModalActive(false);
-  };
-
   // Close modal with Escape key
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      if (e.key === "Escape") closeModal();
-    };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
 
   return (
     <div className={styles.page}>
       {/* Modal */}
-      <div className={`modal ${isModalActive ? "is-active" : ""}`}>
-        <div className="modal-background" onClick={closeModal}></div>
-        <div className="modal-content">
-          <figure className="image">
-            <Image
-              src="/photos/expo.jpg"
-              alt="Expo"
-              width={1200}
-              height={800}
-              style={{ maxHeight: "80vh", objectFit: "contain" }}
-              priority
-            />
-          </figure>
-        </div>
-        <button
-          className="modal-close is-large"
-          aria-label="close"
-          onClick={closeModal}
-        ></button>
-      </div>
 
       {/* Rest of page */}
       <NavBar />
